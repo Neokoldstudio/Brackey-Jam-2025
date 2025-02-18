1d3f6b83998f74d987a6f86748ea4c06cc677503
Naga <prodbynaga@gmail.com>
2025-02-18 17:43:58 -0300

    FMOD integration + Glue Shot Sound

---
+++ Brackeys-Jam/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Gun SFX")]
+    [field: SerializeField] public EventReference glueGunShot {  get; private set; }
+    public static FMODEvents instance {  get; private set; }
+
+    public void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events instance in the scene");
+        }
+        instance = this;
+    }
+
+}
